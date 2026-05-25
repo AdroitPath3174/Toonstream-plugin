666abaec32ed08bdee43785794fda209a46c6939
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-25 09:30:39 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.android.library") version "8.1.0" apply false
-    kotlin("android") version "1.9.0" apply false
-}
-
 task<Delete>("clean") {
     delete(rootProject.buildDir)
 }
