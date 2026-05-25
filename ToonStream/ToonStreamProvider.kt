9928b806e8c2c4f23616d18f1bdd07adaa960f6a
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-25 08:57:08 +0530

    Add files via upload

---
+++ ToonStream/ToonStreamProvider.kt
@@ -0,0 +1,46 @@
+package com.toonstream
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.newAnimeLoadResponse
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.SubtitleFile
+
+class ToonStreamProvider : MainAPI() {
+
+    override var mainUrl = "https://toonstream.vip"
+
+    override var name = "ToonStream"
+
+    override val hasMainPage = true
+
+    override var lang = "en"
+
+    override val supportedTypes = setOf(
+        TvType.Anime
+    )
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        return emptyList()
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        return newAnimeLoadResponse(
+            "ToonStream",
+            url,
+            TvType.Anime
+        ) {
+            plot = "ToonStream Extension"
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        return false
+    }
+}
---
+++ ToonStream/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 1
+
+cloudstream {
+    language = "en"
+
+    authors = listOf(
+        "Sunil"
+    )
+
+    status = 1
+
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = "https://toonstream.vip/favicon.ico"
+
+    description = "ToonStream Extension"
+}
