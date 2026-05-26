c911ff08132c020caead675304ee92997b1e9637
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-26 22:47:11 +0530

    Update ToonStreamProvider.kt

--- ToonStream/ToonStreamProvider.kt
+++ ToonStream/ToonStreamProvider.kt
@@ -2,42 +2,42 @@ package com.toonstream
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.SubtitleFile
 
 class ToonStreamProvider : MainAPI() {
-
     override var mainUrl = "https://toonstream.vip"
-
     override var name = "ToonStream"
-
     override val hasMainPage = true
+    override var lang = "hi"
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(TvType.Anime, TvType.Cartoon)
 
-    override var lang = "en"
-
-    override val supportedTypes = setOf(
-        TvType.Anime
-    )
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        // TODO: Scrape the homepage elements here
+        return newHomePageResponse(emptyList(), hasNext = false)
+    }
 
     override suspend fun search(query: String): List<SearchResponse> {
+        // TODO: Handle search queries
         return emptyList()
     }
 
     override suspend fun load(url: String): LoadResponse {
-        return newAnimeLoadResponse(
-            "ToonStream",
-            url,
-            TvType.Anime
-        ) {
-            plot = "ToonStream Extension"
-        }
+        // TODO: Scrape the details page
+        return AnimeLoadResponse(
+            name = name, 
+            url = url, 
+            apiName = this.name, 
+            type = TvType.Anime
+        )
     }
 
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
+        callback: (ExtractorLink) -> Unit,
+        downloaded: Boolean
     ): Boolean {
-        return false
+        // TODO: Extract the raw .m3u8 or .mp4 links
+        return true
     }
 }
