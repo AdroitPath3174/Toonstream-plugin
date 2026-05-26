4dbcae066213254dcb18496d09facdfe773b7dd2
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-26 22:51:02 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    }
 }
