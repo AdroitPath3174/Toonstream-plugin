603bd387370e8f253e30e71b4cb09cb9a79917f8
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-27 21:55:10 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,18 +6,4 @@ pluginManagement {
         google()
         mavenCentral()
     }
-
-    plugins {
-        id("com.android.application") version "8.0.2"
-        id("org.jetbrains.kotlin.android") version "1.8.20"
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
