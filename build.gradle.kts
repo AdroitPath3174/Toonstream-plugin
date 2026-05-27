edf098f7324d10d79df01dfe5cfe5853278dd063
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-27 09:26:08 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,16 +9,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
     }
 }
 
@@ -64,3 +56,4 @@ subprojects {
 task<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
